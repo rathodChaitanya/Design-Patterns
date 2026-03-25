219eba6cd4d986990962d434375246ba701f47bd
ChaitanyaR2025 <chaitanya@mobeepay.net>
2026-03-25 12:53:57 +0530

    diffence with pattern

--- DesignPattern/src/Main.java
+++ DesignPattern/src/Main.java
@@ -11,5 +11,102 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
+/*        | Type       | Focus                | Problem Solved            |
+                | ---------- | -------------------- | ------------------------- |
+                | Creational | Object creation      | How objects are created   |
+                | Structural | Object relationships | How objects are connected |
+                | Behavioral | Object communication | How objects interact      |
+
+
+        1️⃣ Creational Patterns
+
+       # Instead of creating objects directly with new, these patterns control or simplify object creation.
+
+       # When to Use
+         Use Creational patterns when:
+         Object creation logic is complex
+         You want to control object creation
+         You want to hide creation logic
+         You want to reuse objects
+
+       # Common Creational Patterns
+         Singleton
+         Factory Method
+         Abstract Factory
+         Builder
+         Prototype
+
+
+         2️⃣ Structural Patterns
+
+       # How classes and objects are organized or connected.
+
+       # When to Use
+       Use structural patterns when:
+       You need to connect incompatible systems
+       You want clean architecture
+       You want to simplify complex systems
+
+       # Common Structural Patterns
+       Adapter
+       Decorator
+       Facade
+       Proxy
+       Composite
+       Bridge
+       Flyweight
+
+
+
+
+       3️⃣ Behavioral Patterns
+
+       # How objects communicate with each other.
+
+        # When to Use
+
+       Use behavioral patterns when:
+       You need flexible object communication
+       You want replaceable algorithms
+       You want to handle events or workflows
+
+
+        # Common Behavioral Patterns
+       Strategy
+       Observer
+       Command
+       State
+       Template Method
+       Chain of Responsibility
+       Mediator
+       Visitor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                */
+
     }
 }
